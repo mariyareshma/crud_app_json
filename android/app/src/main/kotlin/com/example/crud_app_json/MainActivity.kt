baac929ed6e3e1d579193ed31bf5d6afc523eff8
Mariya Reshma <rreshma10653@gmail.com>
2023-02-22 12:01:48 +0530

    created app icon and main page and app page

---
+++ android/app/src/main/kotlin/com/example/crud_app_json/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crud_app_json
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'crud_app.dart';
+
+void main() {
+  runApp(const CrudApp());
+}
